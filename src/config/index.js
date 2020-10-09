be7fdd0b86652ac2b6755c9653e4b9da96af878d
Saleh Aly <salehaly01@gmail.com>
2020-10-09 18:45:01 +0200

    Update server port

--- src/config/index.js
+++ src/config/index.js
@@ -9,7 +9,7 @@ const baseConfig = {
   env,
   isDev: env === 'development',
   isTest: env === 'testing',
-  port: 3000,
+  port: 3001,
   secrets: {
     jwt: process.env.JWT_SECRET,
     jwtExp: '100d',
